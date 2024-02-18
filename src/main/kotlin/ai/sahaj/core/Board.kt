a40c5d6642320abc683d1debd2dc5369387aa77c
Sathiyaraman M <sathiyaramanm@sahaj.ai>
2024-02-18 16:15:39 +0530

    Support adding a ladder in Board

--- src/main/kotlin/ai/sahaj/core/Board.kt
+++ src/main/kotlin/ai/sahaj/core/Board.kt
@@ -1,5 +1,16 @@
 package ai.sahaj.core
 
+import ai.sahaj.ai.sahaj.core.exceptions.LadderSameStartAndEndException
+
 class Board(val size: Int = 100) {
+    private val ladders = mutableMapOf<Int, Int>()
+
+    fun addLadder(start: Int, end: Int) {
+        ladders[start] = end
+    }
+
+    fun getLadders(): List<Pair<Int, Int>> {
+        return ladders.map { x -> Pair(x.key, x.value) }
+    }
 
 }
--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -15,6 +15,13 @@ class BoardTest {
         val board = Board(size = 150)
         val size = board.size
         assertEquals(150, size)
+    }
 
+    @Test
+    fun `should be able to add ladders to the board`() {
+        val board = Board()
+        board.addLadder(start = 12, end = 27)
+        val ladders = board.getLadders()
+        assert(ladders.any { x -> x.first == 12 && x.second == 27 })
     }
 }
